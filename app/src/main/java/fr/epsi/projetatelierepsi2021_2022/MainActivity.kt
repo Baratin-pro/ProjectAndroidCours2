41e656531074cbe10ffea6e59789ab191982b50e
EnzoCologni <enzocologni@orange.fr>
2022-02-12 18:11:55 +0100

    PAE-1:Add Created Account

--- app/src/main/java/fr/epsi/projetatelierepsi2021_2022/MainActivity.kt
+++ app/src/main/java/fr/epsi/projetatelierepsi2021_2022/MainActivity.kt
@@ -2,10 +2,13 @@ package fr.epsi.projetatelierepsi2021_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setHeaderTitle("Création de compte")
     }
 }
