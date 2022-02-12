f1ea2408a9112c0e74beabfdb7db1eed7e2fa00d
EnzoCologni <enzocologni@orange.fr>
2022-02-12 19:46:06 +0100

    PAE-4: Add Save data account

--- app/src/main/java/fr/epsi/projetatelierepsi2021_2022/LoginActivity.kt
+++ app/src/main/java/fr/epsi/projetatelierepsi2021_2022/LoginActivity.kt
@@ -1,5 +1,7 @@
 package fr.epsi.projetatelierepsi2021_2022
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,8 +14,41 @@ class LoginActivity : BaseActivity() {
         setContentView(R.layout.activity_login)
         setHeaderTitle("Création de compte")
         showBack()
+        val saveEmail:EditText= findViewById(R.id.editEmail)
+        val savePrenom:EditText= findViewById(R.id.editPrenom)
+        val saveNom:EditText= findViewById(R.id.editNom)
+        val saveAdresse:EditText= findViewById(R.id.editAddress)
+        val saveCodePostal:EditText= findViewById(R.id.editCodePostal)
+        val saveVille:EditText= findViewById(R.id.editVille)
+        val saveCarte:EditText= findViewById(R.id.editCarte)
         val buttonSubmit:Button = findViewById(R.id.buttonSubmitForm)
+        showBack()
+        buttonSubmit.setOnClickListener(View.OnClickListener {
+            writeSharedPref("Prénom",savePrenom.text.toString())
+            writeSharedPref("Nom",saveNom.text.toString())
+            writeSharedPref("Email",saveEmail.text.toString())
+            writeSharedPref("Adresse",saveAdresse.text.toString())
+            writeSharedPref("CodePostal",saveCodePostal.text.toString())
+            writeSharedPref("Ville",saveVille.text.toString())
+            writeSharedPref("Carte",saveCarte.text.toString())
+        })
 
+        /*saveEmail.setText(readSharedPref("email"))
+        savePrenom.setText(readSharedPref("prenom"))
+        saveNom.setText(readSharedPref("nom")) */
+    }
+
+
+    fun writeSharedPref(key:String,value:String){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("account", Context.MODE_PRIVATE)
+        val editor =sharedPreferences.edit()
+        editor.putString(key,value)
+        editor.apply()
+    }
+
+    /*fun readSharedPref(key:String):String{
+        val sharedPreferences: SharedPreferences = getSharedPreferences("account", Context.MODE_PRIVATE)
+        return sharedPreferences.getString(key,"not found").toString()
+    } */
 
     }
-}
